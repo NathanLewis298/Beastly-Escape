9dd2ab8004a8e0e1df2a5512a4b3311005a0be24
Big_Earl <477117@chrome.escg.ac.uk>
2021-03-24 23:22:00 +0000

    Fixed issue of no matches on the board with the new reshuffle feature to ensure theres always at least one, New bomb chaining (Only Column and row)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -425,7 +425,7 @@ public class Board : MonoBehaviour
     {
         RefillBoard();
         yield return new WaitForSeconds(.5f);
-
+        
         while (MatchesOnBoard())
         {
             yield return new WaitForSeconds(.5f);
@@ -435,9 +435,171 @@ public class Board : MonoBehaviour
         findMatches.currentMatches.Clear();
         currentDot = null;
         yield return new WaitForSeconds(.5f);
+
+        if (IsDeadlocked())
+        {
+            ShuffleBoard();
+            Debug.Log("DeadLocked ! ! !");
+        }
         currentState = Gamestate.move;
     
     
+    }
+
+
+    private void SwitchPieces(int column, int row, Vector2 direction)
+    {
+
+        //Take the second piece and save it in a holder
+        GameObject holder = allDots[column + (int)direction.x, row + (int)direction.y] as GameObject;
+        //Switching the first dot to be the second position
+        allDots[column + (int)direction.x, row + (int)direction.y] = allDots[column, row];
+        //Set the first dot to be the second dot.
+        allDots[column, row] = holder;
+
+
+    }
+
+
+    private bool CheckForMatches()
+    {
+        for (int i = 0; i < width; i ++)
+        {
+            for(int j = 0; j < height; j ++)
+            {
+                if(allDots[i,j]!= null)
+                {
+                    //Make sure that one and two to the right are in the board.
+                    if(i < width - 2)
+                    {
+                        //Check if the dots do the right and two to the right exist.
+                        if (allDots[i + 1, j] != null && allDots[i + 2, j] != null)
+                        {
+                            if (allDots[i + 1, j].tag == allDots[i, j].tag && allDots[i + 2, j].tag == allDots[i, j].tag)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                   
+
+                    if(j < height - 2)
+                    {
+                        //Check if the dots above exist.
+                        if (allDots[i, j + 1] != null && allDots[i, j + 2] != null)
+                        {
+                            if (allDots[i, j + 1].tag == allDots[i, j].tag && allDots[i, j + 2].tag == allDots[i, j].tag)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                   
+                
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private bool SwitchAndCheck(int column, int row, Vector2 direction)
+    {
+        SwitchPieces(column, row, direction);
+        if (CheckForMatches())
+        {
+            SwitchPieces(column, row, direction);
+            return true;
+        }
+
+        SwitchPieces(column, row, direction);
+        return false;
+    
+    
+    }
+
+    private bool IsDeadlocked()
+    {
+        
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j ++)
+            {
+                if(allDots[i,j]!= null)
+                {
+                    if(i < width - 1)
+                    {
+                        if(SwitchAndCheck(i, j, Vector2.right))
+                        {
+                            return false;
+                        }
+                    }
+                    if(j < height - 1)
+                    {
+                        if (SwitchAndCheck(i, j, Vector2.up))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+    private void ShuffleBoard()
+    {
+        //Create a list of game objects
+        List<GameObject> newBoard = new List<GameObject>();
+        //Add every piece to this list.
+        for (int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                if(allDots[i,j]!= null)
+                {
+                    newBoard.Add(allDots[i, j]);
+                }
+            }
+        }
+        //For every spot on the board . . .
+        for (int i = 0; i < width; i ++)
+        {
+            for (int j = 0; j < height; j ++)
+            {
+                //If this spot shouldn't be blank
+                if (!blankSpaces[i, j])
+                {
+                    //Pick a random number
+                    int pieceToUse = Random.Range(0, newBoard.Count);
+                    
+                    //Assign the column to the piece.
+                    int maxIterations = 0;
+                    
+                    while (MatchesAt(i, j, newBoard[pieceToUse]) && maxIterations < 100)
+                    {
+                        pieceToUse = Random.Range(0, newBoard.Count);
+                        maxIterations++;
+                    }
+                    //Make a container for the piece.
+                    Dot piece = newBoard[pieceToUse].GetComponent<Dot>();
+                    maxIterations = 0;
+                    piece.column = i;
+                    //Assign the row to the piece.
+                    piece.row = j;
+                    //Fill in the dots array with this new piece.
+                    allDots[i, j] = newBoard[pieceToUse];
+                    //Remove it from the list.
+                    newBoard.Remove(newBoard[pieceToUse]);
+                }
+            }
+        }
+        //Check if its still deadlocked.
+        if (IsDeadlocked())
+        {
+            ShuffleBoard();
+        }
     }
 
 }
