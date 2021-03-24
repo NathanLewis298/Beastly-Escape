e5c3e8bdfb7b309fbb506f05d68dd62d042e2de9
Big_Earl <477117@chrome.escg.ac.uk>
2021-03-24 15:53:37 +0000

    New working Music button to mute it. New power up art and first stage of the UI Manager script for future buttons

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+    public AudioSource Music;
+    public Image MusicImage;
+
+    public Sprite on, off;
+
+
+    public void MuteMusic()
+    {
+        if (Music.isPlaying)
+            Music.Pause();
+        else
+            Music.Play();
+
+        MusicImage.sprite = (Music.isPlaying) ? on : off;
+    }
+
+
+  
+}
